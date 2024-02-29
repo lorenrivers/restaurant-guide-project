6a78da1590bb916d0276c0804efbb3724672aacf
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-02-29 10:10:06 +0000

    initial setup

---
+++ src/app/posts/page.js
@@ -0,0 +1,11 @@
+import { sql } from "@vercel/postgres";
+
+export default async function PostsPage() {
+  const posts = await sql`SELECT * FROM posts`;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+    </div>
+  );
+}
